fc4c37b504dbfcbb12085e500be4aacade791e05
dansssssllllllll <gujogujo18@gmail.com>
2025-04-23 07:50:00 +0800

    Add files via upload

---
+++ script/commands/system/database.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const dbFile = './database.js';
+
+if (!fs.existsSync(dbFile)) fs.writeFileSync(dbFile, '{}');
+
+const getDB = () => JSON.parse(fs.readFileSync(dbFile));
+const saveDB = (db) => fs.writeFileSync(dbFile, JSON.stringify(db));
+
+module.exports = {
+  getBalance: (userID) => getDB()[userID] || 0,
+  setBalance: (userID, amount) => {
+    let db = getDB();
+    db[userID] = amount;
+    saveDB(db);
+  },
+  addBalance: (userID, amount) => {
+    let db = getDB();
+    db[userID] = (db[userID] || 0) + amount;
+    saveDB(db);
+  },
+  subtractBalance: (userID, amount) => {
+    let db = getDB();
+    db[userID] = (db[userID] || 0) - amount;
+    saveDB(db);
+  }
+};
